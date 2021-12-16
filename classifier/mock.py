cc099aee8a4042dd2ba0a570415db63934b5b8f4
Dan Segev <dan.segev@weizmann.ac.il>
2021-12-16 13:58:17 +0200

    working even better!

--- classifier/mock.py
+++ classifier/mock.py
@@ -12,30 +12,31 @@ class Mock(nn.Module):
         super().__init__()
         embedding_dim = 768
 
-        self.backbone = Backbone(img_size=(496, 1024), embedding_dim=embedding_dim)
-        embed_layer = partial(MyHybridEmbed, backbone=self.backbone)
         self.model = VisionTransformer(img_size=(37*496, 1024), patch_size=(embedding_dim, 1), in_chans=3, num_classes=2,
                                        embed_dim=embedding_dim, depth=12, num_heads=12, mlp_ratio=4., qkv_bias=True,
                                        representation_size=None, distilled=False, drop_rate=0., attn_drop_rate=0.,
-                                       drop_path_rate=0., embed_layer=embed_layer, norm_layer=None, act_layer=None,
+                                       drop_path_rate=0., embed_layer=Backbone, norm_layer=None, act_layer=None,
                                        weight_init='')
 
     def forward(self, x):
-        y = self.model(x)  # TODO: NEED TO MAKE THIS EXECUTE SO I CAN CLASSIFY!!
+        y = self.model(x)
+        return y
 
 
 class Backbone(nn.Module):
-    def __init__(self, img_size, embedding_dim):
+    def __init__(self, img_size, patch_size, in_chans, embed_dim):
         super().__init__()
         self.img_height, self.img_width = img_size
-        self.resnet = resnet18(pretrained=False, num_classes=embedding_dim)
+        self.resnet = resnet18(pretrained=False, num_classes=embed_dim)
+        self.num_patches = 37
 
     def forward(self, x):
-        batch_size, channels, overall_height, width = x.shape
-        slices = overall_height // self.img_height
+        batch_size, slices, channels, height, width = x.shape
 
-        x = x.reshape(batch_size, channels, slices, height, width)
-        x = x.permute(0, 2, 1, 3, 4)
+        # slices = overall_height // self.img_height
+        #
+        # x = x.reshape(batch_size, channels, slices, height, width)
+        # x = x.permute(0, 2, 1, 3, 4)
         x = x.reshape(batch_size * slices, channels, height, width)
         x = self.resnet(x)
         x = x.reshape(batch_size, slices, -1)
@@ -89,8 +90,8 @@ if __name__ == '__main__':
     channels = 3
     slices = 37
     x = torch.rand((batch_size, slices, channels, height, width))
-    x = x.permute(0, 2, 1, 3, 4)
-    x = x.reshape(batch_size, channels, slices * height, width)  # have one big image
+    # x = x.permute(0, 2, 1, 3, 4)
+    # x = x.reshape(batch_size, channels, slices * height, width)  # have one big image
 
     mock = Mock()
     y = mock.forward(x)
