a5ab9fc43cad07726e74ec4e3ee1b6e7fbde2cd3
Dan Segev <dan.segev@weizmann.ac.il>
2021-12-16 14:05:39 +0200

    ordered models. ViT in place

---
+++ classifier/models/vit.py
@@ -0,0 +1,35 @@
+import torch
+from timm.models.vision_transformer import VisionTransformer
+from torchvision.models import resnet18
+
+
+class MyViT(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        embedding_dim = 768
+
+        self.model = VisionTransformer(img_size=(37*496, 1024), patch_size=(embedding_dim, 1), in_chans=3, num_classes=2,
+                                       embed_dim=embedding_dim, depth=12, num_heads=12, mlp_ratio=4., qkv_bias=True,
+                                       representation_size=None, distilled=False, drop_rate=0., attn_drop_rate=0.,
+                                       drop_path_rate=0., embed_layer=Backbone, norm_layer=None, act_layer=None,
+                                       weight_init='')
+
+    def forward(self, x):
+        y = self.model(x)
+        return y
+
+
+class Backbone(torch.nn.Module):
+    def __init__(self, img_size, patch_size, in_chans, embed_dim):
+        super().__init__()
+        self.resnet = resnet18(pretrained=False, num_classes=embed_dim)
+        self.num_patches = 37
+
+    def forward(self, x):
+        batch_size, slices, channels, height, width = x.shape
+
+        x = x.reshape(batch_size * slices, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, slices, -1)
+
+        return x
