dc4ec3e29a65da3e9031d10299994a6b575de67f
guylu <guylutsker96@gmail.com>
2022-01-02 14:51:03 +0200

    configured a config file

--- breakdown/distilation_trials/kermany_net.py
+++ breakdown/distilation_trials/kermany_net.py
@@ -11,13 +11,14 @@ class MyViT(torch.nn.Module):
 
         self.config = config
         backbone = partial(Backbone)
-        self.model = VisionTransformer(img_size=(496, 512), patch_size=(16, 16),
-                                       in_chans=3, num_classes=4,
-                                       embed_dim=768, depth=12, num_heads=12, mlp_ratio=4.,
-                                       qkv_bias=True,
-                                       representation_size=None, distilled=False, drop_rate=0., attn_drop_rate=0.,
+        self.model = VisionTransformer(img_size=(496, 512), patch_size=config.vit_patch_size,
+                                       in_chans=3, num_classes=config.vit_num_classes,
+                                       embed_dim=config.vit_embed_dim, depth=config.vit_depth,
+                                       num_heads=config.vit_num_heads, mlp_ratio=config.vit_mlp_ratio,
+                                       qkv_bias=True, representation_size=None, distilled=False,
+                                       drop_rate=config.vit_drop_rate, attn_drop_rate=config.vit_attn_drop_rate,
                                        drop_path_rate=0., embed_layer=backbone, norm_layer=None, act_layer=None,
-                                       weight_init='')  # TODO: Migrate many of these to config.py
+                                       weight_init='')
 
     def forward(self, x):
         return self.model(x)
@@ -27,7 +28,7 @@ class Backbone(torch.nn.Module):
     def __init__(self, embed_dim, num_patches=30, **kwargs):
         super().__init__()
         self.num_patches = 64
-        self.resnet = resnet18(pretrained=False, num_classes=embed_dim*self.num_patches)  # TODO: pretrained on ImageNet?
+        self.resnet = resnet18(num_classes=embed_dim * self.num_patches, pretrained=self.config.backbone_pretrained)
 
     def forward(self, x):
         batch_size, channels, height, width = x.shape
--- breakdown/distilation_trials/train.py
+++ breakdown/distilation_trials/train.py
@@ -13,6 +13,7 @@ import numpy as np
 import argparse
 import cv2 as cv
 from tqdm import tqdm
+from torch.optim.lr_scheduler import StepLR
 
 
 class dot_dict(dict):
@@ -23,10 +24,27 @@ class dot_dict(dict):
 
 
 config = dot_dict({
+    "architecture": "vit",
+    "seed": 42,
     "lr": 0.001,
     "epochs": 2,
     "batch_size": 256,
-    "architecture": "vit"
+    'optimizer': "sgd",
+    "mom": 0.9,
+    'scheduler': "lr",
+    "scheduler_gamma": 0.9,
+    "scheduler_step_size": 1,
+    "backbone_pretrained": False,
+    "vit_patch_size": (16, 16),
+    "vit_num_classes": 4,
+    "vit_embed_dim": 768,
+    "vit_depth": 12,
+    "vit_num_heads": 12,
+    "vit_mlp_ratio": 4.,
+    "vit_drop_rate": 0.2,
+    "vit_num_patches": 64,
+    "vit_attn_drop_rate": 0.
+
 })
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -34,10 +52,10 @@ print(f"running on {device}")
 wandb.login()
 wandb.init(project="my-test-project", entity="guylu", config=config, name="vit trial")
 torch.backends.cudnn.deterministic = True
-random.seed(hash("setting random seeds") % 2 ** 32 - 1)
-np.random.seed(hash("improves reproducibility") % 2 ** 32 - 1)
-torch.manual_seed(hash("by removing stochasticity") % 2 ** 32 - 1)
-torch.cuda.manual_seed_all(hash("so runs are repeatable") % 2 ** 32 - 1)
+random.seed(hash("setting random seeds") % config.seed)
+np.random.seed(hash("improves reproducibility") % config.seed)
+torch.manual_seed(hash("by removing stochasticity") % config.seed)
+torch.cuda.manual_seed_all(hash("so runs are repeatable") % config.seed)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build Kermany dataset')
@@ -51,23 +69,35 @@ if __name__ == '__main__':
     print("getting traning set")
     trainset = kermany_dataset.Kermany_DataSet(args.train[0])
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=config.batch_size,
-                                              shuffle=True, num_workers=0)# drop_last=True
+                                              shuffle=True, num_workers=0)  # drop_last=True
     print("getting validation set")
     valset = kermany_dataset.Kermany_DataSet(args.val[0])
     valloader = torch.utils.data.DataLoader(valset, batch_size=config.batch_size,
                                             shuffle=False, num_workers=0)
     print("starting network")
-    # config = 0
+
+    criterion = nn.CrossEntropyLoss()  # nn.L1Loss
+
     if config.architecture == "vit":
         model = kermany_net.MyViT().to(device)
     elif config.architecture == "res":
         model = kermany_net.Resnet(4).to(device)
+
     wandb.watch(model)
-    criterion = nn.CrossEntropyLoss()  # nn.L1Loss
-    optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=0.9)
+
+    if config.optimizer == "sgd":
+        optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=config.mom)
+    elif config.optimizer == "adam":
+        optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=config.mom)
+
+    if config.scheduler == "lr":
+        scheduler = StepLR(optimizer, step_size=config.scheduler_step_size, gamma=config.scheduler_gamma)
+
+    #############################################################################
     for epoch in range(config.epochs):  # loop over the dataset multiple times
         running_loss = 0.0
         print(f"epoch:{epoch}")
+        scheduler.step()
         for i, data in enumerate(tqdm(trainloader), 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
@@ -87,14 +117,14 @@ if __name__ == '__main__':
             # print statistics
             running_loss += loss.item()
             if i % 20 == 0:  # print every 2000 mini-batches
-                wandb.log({"epoch": epoch, "train loss": loss, "train acc": acc})
+                wandb.log({"epoch": epoch, "train loss": loss, "train acc": acc, "lr": optimizer.lr})
                 if i % 100 == 0:
                     print('[%d, %5d] loss: %.3f' %
                           (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
     print('Finished Training')
-
+    correct = 0
     with torch.no_grad():
         running_loss = 0.0
         for i, data in enumerate(tqdm(trainloader), 0):
@@ -107,12 +137,16 @@ if __name__ == '__main__':
             loss = criterion(outputs, labels)
             outputs = torch.argmax(outputs, dim=1)
             acc = (outputs == labels).sum().item() / inputs.shape[0]
+            correct += (outputs == labels).sum().item()
             wandb.log({"val loss": loss, "val acc": acc})
 
             # print statistics
             running_loss += loss.item()
 
+    wandb.log({"Final Accuracy": correct / len(valset)})
     print('Finished Validating')
+    print()
     print("Finito La Cola")
+
     # model.to_onnx()
     # wandb.save("model.onnx")
