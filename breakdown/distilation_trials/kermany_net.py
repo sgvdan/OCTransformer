c9b26f2d314ac410fca259781f24f79cb22d9596
guylu <guylutsker96@gmail.com>
2022-01-02 12:45:47 +0200

    adding vit support

--- breakdown/distilation_trials/kermany_net.py
+++ breakdown/distilation_trials/kermany_net.py
@@ -13,7 +13,7 @@ class MyViT(torch.nn.Module):
         backbone = partial(Backbone)
         self.model = VisionTransformer(img_size=(496, 512), patch_size=(16, 16),
                                        in_chans=3, num_classes=4,
-                                       embed_dim=12, depth=12, num_heads=12, mlp_ratio=4.,
+                                       embed_dim=768, depth=12, num_heads=12, mlp_ratio=4.,
                                        qkv_bias=True,
                                        representation_size=None, distilled=False, drop_rate=0., attn_drop_rate=0.,
                                        drop_path_rate=0., embed_layer=backbone, norm_layer=None, act_layer=None,
@@ -26,13 +26,13 @@ class MyViT(torch.nn.Module):
 class Backbone(torch.nn.Module):
     def __init__(self, embed_dim, num_patches=30, **kwargs):
         super().__init__()
-        self.resnet = resnet18(pretrained=False, num_classes=embed_dim)  # TODO: pretrained on ImageNet?
-        self.num_patches = 64 # TODO: Should be flexible # of slices per volume
+        self.num_patches = 64
+        self.resnet = resnet18(pretrained=False, num_classes=embed_dim*self.num_patches)  # TODO: pretrained on ImageNet?
 
     def forward(self, x):
         batch_size, channels, height, width = x.shape
 
-        x = x.reshape(batch_size , channels, height, width)
+        x = x.reshape(batch_size, channels, height, width)
         x = self.resnet(x)
         x = x.reshape(batch_size, self.num_patches, -1)
 
