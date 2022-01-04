63d643d5f2501a812693ece82b18fb511ab69265
guylu <guylutsker96@gmail.com>
2022-01-04 21:20:20 +0200

    testing sweeps

--- breakdown/testings/train.py
+++ breakdown/testings/train.py
@@ -15,6 +15,7 @@ from fashion_data import Kermany_DataSet
 
 import wandb
 import os
+from torchvision.models import resnet18
 
 class dot_dict(dict):
     """dot.notation access to dictionary attributes"""
@@ -22,69 +23,35 @@ class dot_dict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+
 hyperparameter_defaults = dict(
-    dropout = 0.5,
-    channels_one = 16,
-    channels_two = 32,
-    batch_size = 100,
-    learning_rate = 0.001,
-    epochs = 2,
-    )
+    batch_size=100,
+    learning_rate=0.001,
+    epochs=2,
+)
 
 wandb.init(config=hyperparameter_defaults, project="pytorch-cnn-fashion")
 config = wandb.config
 
-class CNNModel(nn.Module):
-    def __init__(self):
-        super(CNNModel, self).__init__()
 
-        # Convolution 1
-        self.cnn1 = nn.Conv2d(in_channels=3, out_channels=config.channels_one, kernel_size=5, stride=1, padding=0)
-        self.relu1 = nn.ReLU()
-        # Max pool 1
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
-
-        # Convolution 2
-        self.cnn2 = nn.Conv2d(in_channels=config.channels_one, out_channels=config.channels_two, kernel_size=5, stride=1, padding=0)
-        self.relu2 = nn.ReLU()
-
-        # Max pool 2
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
-
-        self.dropout = nn.Dropout(p=config.dropout)
-
-        # Fully connected 1 (readout)
-        self.fc1 = nn.Linear(config.channels_two*4*4, 4)
+class Resnet18(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = resnet18(pretrained=pretrained, num_classes=num_classes)
 
     def forward(self, x):
-        # Convolution 1
-        out = self.cnn1(x)
-        out = self.relu1(out)
+        batch_size, channels, height, width = x.shape
 
-        # Max pool 1
-        out = self.maxpool1(out)
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
 
-        # Convolution 2
-        out = self.cnn2(out)
-        out = self.relu2(out)
+        return x
 
-        # Max pool 2
-        out = self.maxpool2(out)
-
-        # Resize
-        # Original size: (100, 32, 7, 7)
-        # out.size(0): 100
-        # New out size: (100, 32*7*7)
-        out = out.view(out.size(0), -1)
-        out = self.dropout(out)
-        # Linear function (readout)
-        out = self.fc1(out)
-
-        return out
 
 def main():
-    normalize = transforms.Normalize(mean=[x/255.0 for x in [125.3, 123.0, 113.9]],
-                                         std=[x/255.0 for x in [63.0, 62.1, 66.7]])
+    normalize = transforms.Normalize(mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
+                                     std=[x / 255.0 for x in [63.0, 62.1, 66.7]])
 
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.1307,), (0.3081,))])
@@ -119,8 +86,7 @@ def main():
                                               batch_size=config.batch_size,
                                               shuffle=False)
 
-
-    model = CNNModel()
+    model = Resnet18(4)
     wandb.watch(model)
 
     criterion = nn.CrossEntropyLoss()
@@ -173,7 +139,7 @@ def main():
                     correct += (predicted == labels).sum()
 
                     for label in range(4):
-                        correct_arr[label] += (((predicted == labels) & (labels==label)).sum())
+                        correct_arr[label] += (((predicted == labels) & (labels == label)).sum())
                         total_arr[label] += (labels == label).sum()
 
                 accuracy = correct / total
@@ -188,5 +154,6 @@ def main():
                 print('Iteration: {0} Loss: {1:.2f} Accuracy: {2:.2f}'.format(iter, loss, accuracy))
     torch.save(model.state_dict(), os.path.join(wandb.run.dir, "model.pt"))
 
+
 if __name__ == '__main__':
-   main()
+    main()
