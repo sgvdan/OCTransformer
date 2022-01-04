62dfcfaa3591fd35caf3a5da552098150bfd8ee5
guylu <guylutsker96@gmail.com>
2022-01-04 21:18:14 +0200

    testing sweeps

--- breakdown/testings/train.py
+++ breakdown/testings/train.py
@@ -39,7 +39,7 @@ class CNNModel(nn.Module):
         super(CNNModel, self).__init__()
 
         # Convolution 1
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=config.channels_one, kernel_size=5, stride=1, padding=0)
+        self.cnn1 = nn.Conv2d(in_channels=3, out_channels=config.channels_one, kernel_size=5, stride=1, padding=0)
         self.relu1 = nn.ReLU()
         # Max pool 1
         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
