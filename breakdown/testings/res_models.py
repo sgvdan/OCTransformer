5ae12f5029048aff1811b69d46d702eb7966fb5e
guylu <guylutsker96@gmail.com>
2022-01-08 20:08:26 +0200

    tiding up

---
+++ breakdown/testings/res_models.py
@@ -0,0 +1,105 @@
+import torch
+from torchvision.models import resnet18, resnet50, resnet101, resnet152, wide_resnet50_2, wide_resnet101_2
+import torch.nn as nn
+
+class Resnet18(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        # self.resnet = resnet18(pretrained=pretrained, num_classes=num_classes)
+        self.resnet = resnet18(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
+
+
+class Resnet50(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = resnet50(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
+
+
+class Resnet101(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = resnet101(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
+
+
+class Resnet152(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = resnet152(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
+
+
+class Wide_Resnet50_2(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = wide_resnet50_2(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
+
+
+class Wide_Resnet101_2(torch.nn.Module):
+    def __init__(self, num_classes, pretrained=False):
+        super().__init__()
+        self.resnet = wide_resnet101_2(pretrained=pretrained)
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+
+        x = x.reshape(batch_size, channels, height, width)
+        x = self.resnet(x)
+        x = x.reshape(batch_size, -1)
+
+        return x
--- breakdown/testings/train.py
+++ breakdown/testings/train.py
@@ -5,26 +5,14 @@ Builds a convolutional neural network on the fashion mnist data set.
 Designed to show wandb integration with pytorch.
 """
 
-import torch
-import torch.nn as nn
-import torchvision.transforms as transforms
-import torchvision.datasets as dsets
-from torch.autograd import Variable
-import torch.nn.functional as F
 from data import Kermany_DataSet
-from timm.models.vision_transformer import VisionTransformer
 import timm
 import wandb
 import os
-from torchvision.models import resnet18, resnet50, resnet101, resnet152, wide_resnet50_2, wide_resnet101_2
 
-
-class dot_dict(dict):
-    """dot.notation access to dictionary attributes"""
-    __getattr__ = dict.get
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-
+from utils import *
+from res_models import *
+from model_running import *
 
 hyperparameter_defaults = dict(
     seed=25,
@@ -60,132 +48,6 @@ def init():
         "DRUSEN",
     ]
     return def_args, device, label_names
-
-
-class Resnet18(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        # self.resnet = resnet18(pretrained=pretrained, num_classes=num_classes)
-        self.resnet = resnet18(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-class Resnet50(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        self.resnet = resnet50(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-class Resnet101(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        self.resnet = resnet101(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-class Resnet152(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        self.resnet = resnet152(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-class Wide_Resnet50_2(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        self.resnet = wide_resnet50_2(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-class Wide_Resnet101_2(torch.nn.Module):
-    def __init__(self, num_classes, pretrained=False):
-        super().__init__()
-        self.resnet = wide_resnet101_2(pretrained=pretrained)
-        num_ftrs = self.resnet.fc.in_features
-        self.resnet.fc = nn.Linear(num_ftrs, num_classes)
-
-    def forward(self, x):
-        batch_size, channels, height, width = x.shape
-
-        x = x.reshape(batch_size, channels, height, width)
-        x = self.resnet(x)
-        x = x.reshape(batch_size, -1)
-
-        return x
-
-
-def make_weights_for_balanced_classes(dataset, classes):
-    num_classes = len(classes)
-    num_scans = len(dataset)
-    labels = dataset.get_labels()
-
-    # Count # of appearances per each class
-    count = [0] * num_classes
-    for label in labels:
-        count[int(label)] += 1
-
-    # Each class receives weight in reverse proportion to its # of appearances
-    weight_per_class = [0.] * num_classes
-    for idx in classes:
-        weight_per_class[idx] = float(num_scans) / float(count[idx])
-
-    # Assign class-corresponding weight for each element
-    weights = [0] * num_scans
-    for idx, label in enumerate(labels):
-        weights[idx] = weight_per_class[int(label)]
-
-    return torch.FloatTensor(weights)
 
 
 def Get_Model(config, device):
@@ -245,122 +107,6 @@ def Get_Optimizer(model):
         optimizer = torch.optim.RMSprop(model.parameters(), lr=config.learning_rate, momentum=config.mom,
                                         weight_decay=config.weight_decay)
     return optimizer
-
-
-def Train(criterion, device, label_names, model, optimizer, train_loader, val_loader):
-    iter = 0
-    for epoch in range(3):
-        print(f'epoch: {epoch}')
-        for i, (images, labels) in enumerate(train_loader):
-            # if iter == 501:
-            #     break
-            images = Variable(images).to(device)
-            labels = Variable(labels).to(device)
-
-            # Clear gradients w.r.t. parameters
-            optimizer.zero_grad()
-
-            # Forward pass to get output/logits
-            outputs = model(images)
-
-            # Calculate Loss: softmax --> cross entropy loss
-            loss = criterion(outputs, labels)
-            # Getting gradients w.r.t. parameters
-            loss.backward()
-            # Updating parameters
-            optimizer.step()
-
-            iter += 1
-            if iter % 50 == 0:
-                print(f'iter : {iter}')
-                print(loss)
-                wandb.log({"loss": loss, "epoch": epoch})
-            if iter % 500 == 0:
-                Validation(device, iter, label_names, loss, model, val_loader)
-
-
-def Validation(device, iter, label_names, loss, model, val_loader):
-    # Calculate Accuracy
-    correct = 0.0
-    correct_arr = [0.0] * 10
-    total = 0.0
-    total_arr = [0.0] * 10
-    # Iterate through test dataset
-    with torch.no_grad():
-        for images, labels in val_loader:
-            images = Variable(images).to(device)
-            labels = labels.to(device)
-            # Forward pass only to get logits/output
-            outputs = model(images)
-
-            # Get predictions from the maximum value
-            _, predicted = torch.max(outputs.data, 1)
-
-            # Total number of labels
-            total += labels.size(0)
-            correct += (predicted == labels).sum()
-
-            for label in range(4):
-                correct_arr[label] += (((predicted == labels) & (labels == label)).sum())
-                total_arr[label] += (labels == label).sum()
-
-        accuracy = correct / total
-
-        metrics = {'val accuracy': accuracy}
-        for label in range(4):
-            metrics['Val Accuracy ' + label_names[label]] = correct_arr[label] / total_arr[label]
-
-        wandb.log(metrics)
-
-        # wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None,
-        # y_true = ground_truth, preds = predictions,
-        #                                class_names = class_names)})
-
-        # Print Loss
-        print('Iteration: {0} Loss: {1:.2f} Accuracy: {2:.2f}'.format(iter, loss, accuracy))
-
-
-def Testing(device, label_names, model, test_loader):
-    correct = 0.0
-    correct_arr = [0.0] * 10
-    total = 0.0
-    total_arr = [0.0] * 10
-    predictions = None
-    ground_truth = None
-    # Iterate through test dataset
-    with torch.no_grad():
-        for i, (images, labels) in enumerate(test_loader):
-            images = Variable(images).to(device)
-            labels = labels.to(device)
-            # Forward pass only to get logits/output
-            outputs = model(images)
-
-            # Get predictions from the maximum value
-            _, predicted = torch.max(outputs.data, 1)
-
-            # Total number of labels
-            total += labels.size(0)
-            correct += (predicted == labels).sum()
-
-            for label in range(4):
-                correct_arr[label] += (((predicted == labels) & (labels == label)).sum())
-                total_arr[label] += (labels == label).sum()
-
-            if i == 0:
-                predictions = predicted
-                ground_truth = labels
-            else:
-                predictions = torch.cat((predictions, predicted), 0)
-                ground_truth = torch.cat((ground_truth, labels), 0)
-        accuracy = correct / total
-
-        metrics = {'Test Accuracy': accuracy}
-        for label in range(4):
-            metrics['Test Accuracy ' + label_names[label]] = correct_arr[label] / total_arr[label]
-        wandb.log(metrics)
-        # wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None,
-        #                                                    y_true=ground_truth, preds=predictions,
-        #                                                    class_names=label_names)})
 
 
 def main():
