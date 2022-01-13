76f9d89f1e232beca6d227c879d97583ef00331f
guylu <guylutsker96@gmail.com>
2022-01-13 13:57:05 +0200

    bugs

--- breakdown/testings/train.py
+++ breakdown/testings/train.py
@@ -16,7 +16,7 @@ hyperparameter_defaults = dict(
     optimizer="adam",
     mom=0.9,
     weight_decay=0,
-    architecture='convnext_base',
+    architecture='convmixer_1536_20',
     pretrain=False,
 )
 
