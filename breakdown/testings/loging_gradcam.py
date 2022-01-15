b9ab382a31a4289db574648523da3532ec8ee258
guylu <guylutsker96@gmail.com>
2022-01-15 16:04:03 +0200

    bugs

--- breakdown/testings/loging_gradcam.py
+++ breakdown/testings/loging_gradcam.py
@@ -64,7 +64,7 @@ columns = ["id", "Original Image", "Predicted", "Truth", "GradCAM", 'ScoreCAM', 
 test_dt = wandb.Table(columns=columns)
 
 for i, (images, labels) in enumerate(test_loader):
-    if i == 10:
+    if i == 3:
         break
     images = Variable(images).to(device)
     labels = labels.to(device)
