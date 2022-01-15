51d7ed87d62bfc611aaaece55520139c7b1483f6
guylu <guylutsker96@gmail.com>
2022-01-15 23:17:03 +0200

    vit attention

--- breakdown/testings/vit_att_trial/run_wandb_att.py
+++ breakdown/testings/vit_att_trial/run_wandb_att.py
@@ -154,6 +154,7 @@ for i, (images, labels) in enumerate(test_loader):
     images = Variable(images).to(device)
     labels = labels.to(device)
     # Forward pass only to get logits/output
+    print(images.unsqueeze(0).shape)
     outputs = model(images.unsqueeze(0))
 
     # Get predictions from the maximum value
