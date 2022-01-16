93f00e0dae1ba40a43a0b10f0f5171af9151f1d2
guylu <guylutsker96@gmail.com>
2022-01-16 13:12:18 +0200

    vit attention

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -139,7 +139,7 @@ ground_truth = None
 # Iterate through test dataset
 
 columns = ["id", "Original Image", "Predicted", "Truth", "Attention", "GradCAM", 'ScoreCAM', 'GradCAMPlusPlus',
-           'XGradCAM', 'EigenCAM', 'EigenGradCAM']
+           'XGradCAM', 'EigenCAM', 'EigenGradCAM', 'Avg']
 # for a in label_names:
 #     columns.append("score_" + a)
 test_dt = wandb.Table(columns=columns)
@@ -214,9 +214,14 @@ for i, (images, labels) in enumerate(test_loader):
     gradcam = res
     images = images.squeeze()
     cat = generate_visualization(images)
+
+    sum = cat
+    for j in range(len(gradcam)):
+        sum += gradcam[j]
+    sum = sum / 7
     row = [i, wandb.Image(images), label_names[predicted.item()], label_names[labels.item()],
            wandb.Image(cat), wandb.Image(gradcam[0]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
-           wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4])]
+           wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4]), wandb.Image(sum)]
     test_dt.add_data(*row)
 
     # wandb.log({"conf_mat": wandb.plot.confusion_matrix(probs=None,
