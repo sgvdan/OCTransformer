403f11cc3fab796b0bbdc10177fd5fc079bc1ffc
guylu <guylutsker96@gmail.com>
2022-01-16 10:54:28 +0200

    vit attention

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -194,7 +194,7 @@ for i, (images, labels) in enumerate(test_loader):
     for cam_algo in cams:
         print(images.shape)
         cam = cam_algo(model=model_timm, target_layers=target_layers,
-                       use_cuda=True if torch.cuda.is_available() else False)
+                       use_cuda=True if torch.cuda.is_available() else False, reshape_transform=reshape_transform)
         target_category = labels.item()
         grayscale_cam = cam(input_tensor=images)
         grayscale_cam = grayscale_cam[0, :]
