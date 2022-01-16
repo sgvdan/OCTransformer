eb6987546111bbf9d5f39fbfe96b1c3019a81122
guylu <guylutsker96@gmail.com>
2022-01-16 16:53:21 +0200

    vit attention

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -210,19 +210,19 @@ for i, (images, labels) in enumerate(test_loader):
     cat = generate_visualization(images)
 
     sum = cat
-    # print(type(cat))
-    # print(cat.shape)
-    # print(type(gradcam[0]))
-    # print(gradcam[0].shape)
-    # print((cat.min(), cat.max()))
-    # print((gradcam[0].min(), gradcam[0].max()))
+    print(type(cat))
+    print(cat.shape)
+    print(type(gradcam[0]))
+    print(gradcam[0].shape)
+    print((cat.min(), cat.max()))
+    print((gradcam[0].min(), gradcam[0].max()))
     for j in range(len(gradcam)):
         sum = sum + gradcam[j]
-    #     print(f'i:{j} range:{(sum.min(), sum.max())}')
-    # print((sum.min(), sum.max()))
-    # sum = sum / 7
-    # print((sum.min(), sum.max()))
-    # print(sum.shape)
+        print(f'i:{j} range:{(sum.min(), sum.max())}')
+    print((sum.min(), sum.max()))
+    sum = sum / 7
+    print((sum.min(), sum.max()))
+    print(sum.shape)
     row = [i, wandb.Image(images), label_names[predicted.item()], label_names[labels.item()],
            wandb.Image(cat), wandb.Image(gradcam[0]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
            wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4]), wandb.Image(sum)]
