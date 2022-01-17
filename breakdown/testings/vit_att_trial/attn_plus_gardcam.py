a93f5a19f84f22035be63e99ef671c5106a8208b
guylu <guylutsker96@gmail.com>
2022-01-17 15:38:22 +0200

    vit attention

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -212,10 +212,10 @@ for i, (images, labels) in enumerate(test_loader):
     images = images.squeeze()
     cat = generate_visualization(images)
 
-    sum = cat.copy() - images.cpu().numpy().astype(np.float32)
+    sum = cat.copy() - images.permute(1, 2, 0).cpu().numpy().astype(np.float32)
     sum = sum * 4
     for i in range(len(gradcam)):
-        only_grad = (gradcam[i].astype(np.float32) - images.cpu().numpy().astype(np.float32))
+        only_grad = (gradcam[i].astype(np.float32) - images.permute(1, 2, 0).cpu().numpy().astype(np.float32))
         m = only_grad.max()
         only_grad[only_grad < m / 5] = 0
         sum = sum + only_grad
