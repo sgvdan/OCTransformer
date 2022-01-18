f389c3b50eaffdb4f4d347d2c48929e94f6e2c1c
guylu <guylutsker96@gmail.com>
2022-01-18 11:53:54 +0200

    trying to dino

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -126,7 +126,7 @@ predictions = None
 ground_truth = None
 # Iterate through test dataset
 
-columns = ["id", "Original Image", "Predicted", "Truth", "Correct", "Attention NORMAL", "Attention CNV",
+columns = ["id", "Original Image", "Predicted", "Logits", "Truth", "Correct", "Attention NORMAL", "Attention CNV",
            "Attention DME",
            "Attention DRUSEN", "GradCAM", 'ScoreCAM', 'GradCAMPlusPlus', 'XGradCAM', 'EigenCAM', 'EigenGradCAM', 'Avg']
 # for a in label_names:
@@ -216,7 +216,8 @@ for i, (images, labels) in enumerate(test_loader):
     # plt.show()
     avg = vis
     T = predicted.item() == labels.item()
-    row = [i, wandb.Image(images), label_names[predicted.item()], label_names[labels.item()], T,
+    out = outputs_timm
+    row = [i, wandb.Image(images), label_names[predicted.item()], out.cpu().numpy(), label_names[labels.item()], T,
            wandb.Image(attn_diff_cls[0]), wandb.Image(attn_diff_cls[1]), wandb.Image(attn_diff_cls[2]),
            wandb.Image(attn_diff_cls[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
            wandb.Image(gradcam[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[4]), wandb.Image(avg)]
