d92621057e4faa704620e6c3697f6d6b965df419
guylu <guylutsker96@gmail.com>
2022-01-18 14:21:20 +0200

    trying to dino

--- breakdown/testings/vit_att_trial/attn_plus_gardcam.py
+++ breakdown/testings/vit_att_trial/attn_plus_gardcam.py
@@ -219,7 +219,8 @@ for i, (images, labels) in enumerate(test_loader):
     out = outputs_timm
 
     fig, ax = plt.subplots()
-    ax.bar(label_names, out.cpu().detach().numpy())
+
+    ax.bar(label_names, out.cpu().detach().numpy()[0])
     row = [i, wandb.Image(images), label_names[predicted.item()], fig, label_names[labels.item()], T,
            wandb.Image(attn_diff_cls[0]), wandb.Image(attn_diff_cls[1]), wandb.Image(attn_diff_cls[2]),
            wandb.Image(attn_diff_cls[3]), wandb.Image(gradcam[4]), wandb.Image(gradcam[1]), wandb.Image(gradcam[2]),
