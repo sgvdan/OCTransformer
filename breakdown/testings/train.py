828dbe7c7125bcae6de28ae72c1f60396669f124
guylu <guylutsker96@gmail.com>
2022-01-18 21:45:10 +0200

    trying to dino

--- breakdown/testings/train.py
+++ breakdown/testings/train.py
@@ -159,6 +159,7 @@ def main():
     #########################################################################################################
 
     print("starting training:\n\n")
+    print(config.architecture == "dino")
     Train(criterion, device, label_names, model, optimizer, train_loader, val_loader, config.epochs, test_loader,
           config.architecture == "dino")
 
