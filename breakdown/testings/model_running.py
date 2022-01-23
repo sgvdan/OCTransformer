7b1558ade754e1908008a81eebefd2ae58738eb6
guylu <guylutsker96@gmail.com>
2022-01-23 20:37:26 +0200

    vis features

--- breakdown/testings/model_running.py
+++ breakdown/testings/model_running.py
@@ -148,7 +148,7 @@ def vis_gif(aligned_mapper, colors, embds):
         func=animate,
         frames=num_frames,
         interval=40)
-    anim.save("aligned_umap_pendigits_anim__3.gif", writer="pillow")
+    anim.save("aligned_umap_pendigits_anim____5_1.gif", writer="pillow")
     plt.close(anim._fig)
 
 
