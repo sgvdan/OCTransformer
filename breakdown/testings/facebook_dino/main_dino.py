a03884541f132ec8c534252ea196955cf7a3895a
guylu <guylutsker96@gmail.com>
2022-01-25 22:22:13 +0200

    vis features

--- breakdown/testings/facebook_dino/main_dino.py
+++ breakdown/testings/facebook_dino/main_dino.py
@@ -87,7 +87,7 @@ def get_args_parser():
     parser.add_argument('--clip_grad', type=float, default=3.0, help="""Maximal parameter
         gradient norm if using gradient clipping. Clipping with norm .3 ~ 1.0 can
         help optimization for larger ViT architectures. 0 for disabling.""")
-    parser.add_argument('--batch_size_per_gpu', default=2, type=int,
+    parser.add_argument('--batch_size_per_gpu', default=100, type=int,
         help='Per-GPU batch-size : number of distinct images loaded on one GPU.')
     parser.add_argument('--epochs', default=4, type=int, help='Number of epochs of training.')
     parser.add_argument('--freeze_last_layer', default=1, type=int, help="""Number of epochs
