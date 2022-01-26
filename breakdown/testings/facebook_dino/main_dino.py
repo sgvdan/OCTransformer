aa8fe4724477744fe602c7a6c14f56d4452d6085
guylu <guylutsker96@gmail.com>
2022-01-26 18:20:32 +0200

    vis features

--- breakdown/testings/facebook_dino/main_dino.py
+++ breakdown/testings/facebook_dino/main_dino.py
@@ -120,7 +120,7 @@ def get_args_parser():
     parser.add_argument('--data_path', default="../../../../data/kermany/train", type=str,
         help='Please specify path to the ImageNet training data.')
     parser.add_argument('--output_dir', default="./p8", type=str, help='Path to save logs and checkpoints.')
-    parser.add_argument('--saveckp_freq', default=20, type=int, help='Save checkpoint every x epochs.')
+    parser.add_argument('--saveckp_freq', default=1, type=int, help='Save checkpoint every x epochs.')
     parser.add_argument('--seed', default=0, type=int, help='Random seed.')
     parser.add_argument('--num_workers', default=1, type=int, help='Number of data loading workers per GPU.')
     parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
