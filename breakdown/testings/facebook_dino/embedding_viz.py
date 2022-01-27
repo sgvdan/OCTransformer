0dc7084d9d1d4afe00c8ef7e67758c614d561217
guylu <guylutsker96@gmail.com>
2022-01-27 11:54:56 +0200

    vis features

--- breakdown/testings/facebook_dino/embedding_viz.py
+++ breakdown/testings/facebook_dino/embedding_viz.py
@@ -162,7 +162,7 @@ with torch.no_grad():
         for n in range(9):
             embds[n].append(outputs[n][0, 0, :].flatten().cpu().detach().numpy())
             colors[n].append(labels.item())
-        raise NotImplemented
+        # raise NotImplemented
 for n in range(8, -1, -1):
     print(n)
     embdss = np.array(embds[n])
