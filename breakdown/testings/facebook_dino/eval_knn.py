a748a55053bd5a2b3d0e2cd54911de112c0ef0d0
guylu <guylutsker96@gmail.com>
2022-01-27 10:30:57 +0200

    vis features

--- breakdown/testings/facebook_dino/eval_knn.py
+++ breakdown/testings/facebook_dino/eval_knn.py
@@ -149,9 +149,9 @@ def knn_classifier(train_features, train_labels, test_features, test_labels, k, 
     for idx in range(0, num_test_images, imgs_per_chunk):
         # get the features for test images
         features = test_features[
-            idx : min((idx + imgs_per_chunk), num_test_images), :
-        ]
-        targets = test_labels[idx : min((idx + imgs_per_chunk), num_test_images)]
+                   idx: min((idx + imgs_per_chunk), num_test_images), :
+                   ]
+        targets = test_labels[idx: min((idx + imgs_per_chunk), num_test_images)]
         batch_size = targets.shape[0]
 
         # calculate the dot product and compute top-k neighbors
@@ -191,19 +191,20 @@ class ReturnIndexDataset(datasets.ImageFolder):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Evaluation with weighted k-NN on ImageNet')
     parser.add_argument('--batch_size_per_gpu', default=2, type=int, help='Per-GPU batch-size')
-    parser.add_argument('--nb_knn', default=5, nargs='+', type=int,
-        help='Number of NN to use. 20 is usually working the best.')
+    parser.add_argument('--nb_knn', default=[5, 10, 20, 100, 200], nargs='+', type=int,
+                        help='Number of NN to use. 20 is usually working the best.')
     parser.add_argument('--temperature', default=0.07, type=float,
-        help='Temperature used in the voting coefficient')
-    parser.add_argument('--pretrained_weights', default='checkpoint.pth', type=str, help="Path to pretrained weights to evaluate.")
+                        help='Temperature used in the voting coefficient')
+    parser.add_argument('--pretrained_weights', default='checkpoint.pth', type=str,
+                        help="Path to pretrained weights to evaluate.")
     parser.add_argument('--use_cuda', default=True, type=utils.bool_flag,
-        help="Should we store the features on GPU? We recommend setting this to False if you encounter OOM")
+                        help="Should we store the features on GPU? We recommend setting this to False if you encounter OOM")
     parser.add_argument('--arch', default='vit_base', type=str, help='Architecture')
     parser.add_argument('--patch_size', default=16, type=int, help='Patch resolution of the model.')
     parser.add_argument("--checkpoint_key", default="teacher", type=str,
-        help='Key to use in the checkpoint (example: "teacher")')
+                        help='Key to use in the checkpoint (example: "teacher")')
     parser.add_argument('--dump_features', default=None,
-        help='Path where to save computed features, empty for no saving')
+                        help='Path where to save computed features, empty for no saving')
     parser.add_argument('--load_features', default=None, help="""If the features have
         already been computed, where to find them.""")
     parser.add_argument('--num_workers', default=1, type=int, help='Number of data loading workers per GPU.')
@@ -237,6 +238,6 @@ if __name__ == '__main__':
         print("Features are ready!\nStart the k-NN classification.")
         for k in args.nb_knn:
             top1, top5 = knn_classifier(train_features, train_labels,
-                test_features, test_labels, k, args.temperature)
+                                        test_features, test_labels, k, args.temperature)
             print(f"{k}-NN classifier result: Top1: {top1}, Top5: {top5}")
     dist.barrier()
