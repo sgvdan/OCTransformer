3d98ad8b27eb66916ae545db801961ca31ed7508
guylu <guylutsker96@gmail.com>
2022-01-28 09:28:22 +0200

    vis features

--- breakdown/testings/facebook_dino/visualize_attention.py
+++ breakdown/testings/facebook_dino/visualize_attention.py
@@ -38,7 +38,7 @@ import vision_transformer as vits
 def show_cam_on_image(img, mask):
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
     heatmap = np.float32(heatmap) / 255
-    cam = heatmap * 0.4 + np.float32(img)
+    cam = heatmap * 0.6 + np.float32(img)
     cam = cam / np.max(cam)
     return cam
 
